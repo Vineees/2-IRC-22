9d24e160351682db24a062e2cc40ae43212204ff
Vinicius <marcosvinicius.sm@icloud.com>
2022-09-19 21:53:32 -0300

    A

---
+++ Friends  Help/record.py
@@ -0,0 +1,20 @@
+#Entradas
+input
+
+#Caracteristicas entradas
+int - INteiros
+float - Reais
+str - Texto
+
+#Saidas
+print ('Opa'(nota2 + nota1) /2) or ("")
+
+
+#Variaveis
+
+nota
+cor
+tipo
+nome_gabriel
+
+cor != Cor
