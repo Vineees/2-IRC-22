37fa8ba5b83988f4a5308f2ce67023bcf3718b08
Vinicius <marcosvinicius.sm@icloud.com>
2022-10-22 22:55:38 -0300

    Changes

---
+++ Monitoria/Aula 2/exemplos for/q5.py
@@ -0,0 +1,13 @@
+#Uma loja utiliza o código V para compras à vista e o código P para compras a prazo.
+# Faça um programa em Python que receba o código da compra e o valor de 15 transações.
+# Calcule e mostre:
+# a) O valor total das compras à vista
+# b);  O valor total das compras a prazo;
+# c)  O valor total das compras efetuadas
+
+#Entrada - P ou V
+#Entrada - Valor das Compras
+
+#Saida - Total Compras a Vista
+#SAida - Total Compras a Prazo
+#Saida - Valor Total
