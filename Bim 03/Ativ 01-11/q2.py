64f316e64bf79da7776752aa71c197b7ab5d9cff
Vinee <72805423+Vineees@users.noreply.github.com>
2022-11-01 09:12:22 -0300

    Update q2.py

--- Bim 03/Ativ 01-11/q2.py
+++ Bim 03/Ativ 01-11/q2.py
@@ -1,4 +1,4 @@
-
+#2ª)  Faça um programa que leia uma quantidade indeterminada de números positivos e conte quantos deles estão nos seguintes intervalos: [0-25], [26-50], [51-75] e [76-100]. A entrada de dados deverá terminar quando for lido um número negativo.
 var1 = 0
 var2 = 0
 var3 = 0
