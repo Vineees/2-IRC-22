95c13fd2f6c1da6e4952a7ce298c79b175666e08
Vinee <72805423+Vineees@users.noreply.github.com>
2022-11-01 09:12:02 -0300

    Aula 01/11

--- Bim 03/Ativ 01-11/q1.py
+++ Bim 03/Ativ 01-11/q1.py
@@ -1,3 +1,5 @@
+#1ª)  Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+
 nota = 0
 nota = float(input('Insira Sua Nota:'))
 while nota > 10 or nota < 0:
